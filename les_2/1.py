8666b906bd0f1ab3ee80983ebf473416bd10f5ea
derevianko <yaroslav.derevianko@gmail.com>
2017-10-23 10:26:08 +0300

    Change /les_2/1.py

--- les_2/1.py
+++ les_2/1.py
@@ -1,4 +1,4 @@
-#1 .Write a script which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers.
+# 1 .Write a script which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers.
 #		Sample data : 1, 5, 7, 23
 #		Output :
 #		List : [‘1', ' 5', ' 7', ' 23']
