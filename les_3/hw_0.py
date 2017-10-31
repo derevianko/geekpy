5ffbb3f742736a7f3f08351c94ac7be0402180aa
derevianko <yaroslav.derevianko@gmail.com>
2017-11-01 00:28:26 +0200

    Create 7 task

--- les_3/hw_0.py
+++ les_3/hw_0.py
@@ -1,16 +1,55 @@
-#create func
-str_int = input('Input some valeus:\n >_ ')
-if len(str_int) > 30 and len(str_int) < 50:
-    l_str_int = tuple(str_int)
-    t_str_int = l_str_int.split("'")
-    print(t_str_int)
-    print(sum(l_str_int))
-    print(len(str_int))
-elif len(str_int) < 30:
-#print sum all numbers and all words
-    print('111')
-elif len(str_int) > 50:
-#my idia
-    print('2222')
-else:
-    print('3333')
+# 1 task
+# func seasons
+# create func
+def seasons():
+# input number in month
+    month = int(input('Input number of month:\n >_ '))
+    m_dict = {
+    1: ['winter', 'January'],
+    2: ['winter', 'February'],
+    3: ['spring', 'March'],
+    4: ['spring', 'April'],
+    5: ['spring', 'May'],
+    6: ['summer', 'June'],
+    7: ['summer', 'July'],
+    8: ['summer', 'August'],
+    9: ['autumn', 'September'],
+    10: ['autumn', 'October'],
+    11: ['autumn', 'November'],
+    12: ['winter', 'December'],
+    }
+    print(m_dict.get(month))
+# run func
+seasons()
+
+
+
+# 7 task
+# create func calc
+def calc():
+    opr = str(input('''Select math operation:
+    + for addition
+    - for subtraction
+    * for multiplication
+    / for division
+    >_ '''))
+
+    num_1 = int(input('Enter first number: '))
+    num_2 = int(input('Enter second number: '))
+
+    if opr == '+':
+        print('{} + {} ='.format(num_1, num_2))
+        print(num_1 + num_2)
+    elif opr == '-':
+        print('{} - {} ='.format(num_1, num_2))
+        print(num_1 - num_2)
+    elif opr == '*':
+        print('{} * {} ='.format(num_1, num_2))
+        print(num_1 * num_2)
+    elif opr == '/':
+        print('{} / {} ='.format(num_1, num_2))
+        print(num_1 / num_2)
+    else:
+        print('No selected, run calc againe')
+
+calc()
