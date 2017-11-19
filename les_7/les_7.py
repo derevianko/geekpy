c090c60569dac2c294e823d0db9537836a91a096
derevianko <yaroslav.derevianko@gmail.com>
2017-11-20 01:39:34 +0200

    add coment

--- les_7/les_7.py
+++ les_7/les_7.py
@@ -2,16 +2,19 @@ import argparse
 # import logging
 import requests
 
-# # add filemode="w" to overwrite
+
+# some example code with gide
+# add filemode="w" to overwrite
 # logging.basicConfig(filename="sample.log",filemode=”w”, level=logging.INFO)
 #
 # logging.debug("This is a debug message")
 # logging.info("Informational message")
 # logging.error("An error has happened!")
-#
+
+
+
+# url tmp
 # url = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
-# response = requests.get(url)
-# response(response.json())
 
 parser = argparse.ArgumentParser()
 parser.add_argument('url', help='input parse url', type=str)
@@ -19,7 +22,7 @@ parser.add_argument('url', help='input parse url', type=str)
 args = parser.parse_args()
 url = args.url
 
-# url = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
+
 response = requests.get(url)
 response = response.json()
 for num, x_id in enumerate(response, 0):
